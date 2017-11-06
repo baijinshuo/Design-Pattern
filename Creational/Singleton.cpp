d0adacea27c9624aee59591474522d4795b6747e
Jinshuo Bai <baijinshuo_1209@hotmail.com>
2017-11-06 15:32:58 -0500

    Add files via upload

---
+++ Creational/Singleton.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+/*
+Ensure a class has only one instance, and provide a global point of access to it
+*/
+using namespace std;
+class GlobalClass
+{
+private:
+	int m_value;
+	static GlobalClass *s_instance;
+	GlobalClass(int v = 0)
+	{
+		m_value = v;
+	}
+public:
+	int get_value()
+	{
+		return m_value;
+	}
+	void set_value(int v)
+	{
+		m_value = v;
+	}
+	static GlobalClass *instance()
+	{
+		if (!s_instance)
+		{
+			s_instance = new GlobalClass;
+		}
+		return s_instance;
+	}
+};
+
+GlobalClass *GlobalClass::s_instance = 0;
+
+void foo(void)
+{
+	GlobalClass::instance()->set_value(1);
+	cout << "foo: global_ptr is " << GlobalClass::instance()->get_value() << '\n';
+}
+
+void bar(void)
+{
+	GlobalClass::instance()->set_value(2);
+	cout << "bar: global_ptr is " << GlobalClass::instance()->get_value() << '\n';
+}
+
+int main()
+{
+	cout << "main: global_ptr is " << GlobalClass::instance()->get_value() << '\n';
+	foo();
+	bar();
+	return 0;
+}
